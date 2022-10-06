37520c38511cb929ba2f4d7bb8a121bc32d15f38
Nsi20 <106813645+Nsi20@users.noreply.github.com>
2022-10-06 15:58:09 +0100

    Update pop.c

--- pop.c
+++ pop.c
@@ -1,23 +1,36 @@
 #include "monty.h"
-/**
- * f_pop - prints the top
- * @head: stack head
- * @counter: line_number
- * Return: no return
- */
-void f_pop(stack_t **head, unsigned int counter)
-{
-	stack_t *h;
 
-	if (*head == NULL)
+/**
+ * pop - Function that remove the head of stack
+ * @stack: stack structure
+ * @line_number: Number of instructions
+ */
+void pop(stack_t **stack, unsigned int line_number)
+{
+	stack_t *temp = NULL;
+	int n;
+
+	n = stack_len(*stack);
+	if (n <= 0)
 	{
-		fprintf(stderr, "L%d: can't pop an empty stack\n", counter);
-		fclose(bus.file);
-		free(bus.content);
-		free_stack(*head)
+		fprintf(stderr, "L%d: can't pop an empty stack\n", line_number);
+		if (list_opcode != NULL)
+			free_list_opcode(list_opcode);
+		if (*stack != NULL)
+			free_list_stack(*stack);
 		exit(EXIT_FAILURE);
 	}
-	h = *head;
-	*head = h->next;
-	free(h);
+
+	if ((*stack)->next == NULL)
+	{
+		free(*stack);
+		*stack = NULL;
+		return;
+	}
+
+	temp = *stack;
+	*stack = (*stack)->next;
+	(*stack)->prev = NULL;
+	free(temp);
+	temp = NULL;
 }
